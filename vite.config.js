c63714aae07054413e4f50dbf6f6c491aecb82e0
Darrion Nguyen <darrion0920@gmail.com>
2024-10-24 02:41:00 -0500

    updated vite config to see if multi page works

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { fileURLToPath } from "url"
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "test"
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: 'test'
 })
